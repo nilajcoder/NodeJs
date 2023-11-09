9030f2733077c59c744074f9eac13e536ae5267f
nilajhub <nilaj.chakraborty007@gmail.com>
2023-11-09 20:46:53 +0530

    signupgetpostcompleted

--- FirstNodeApi/app.js
+++ FirstNodeApi/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express ();
 app.use(express.json());
-const port=3000;
+const port = 3000; 
 app.listen(port, () => {
     console.log("Server Listening on PORT:", port);
   });
+/*
+get method
+*/
   app.get("/status", (request, response) => {
     const status = {
        "Status": "Running"
@@ -14,21 +17,48 @@ app.listen(port, () => {
  });
 
  /*
- post method
+ post method for sign in
  */
  app.post("/signup", (request, response) => {
  
-   const { username, password } = request.body;
+   const {
+      id,
+      user_name, 
+      first_name,
+      middle_name,
+      last_name,
+      mobile_no,
+      email,
+      password,
+      account_confirmation,
+      reset_pass,
+      designation,
+      is_active,
+      is_deleted,
+      created_by,
+      created_date,
+      updated_by,
+      updated_date,
+      img,
+      is_first_login,
+      is_boldbi_user,
+      roleId,
+      clientId
+      
+   } = request.body;
    
 
    response.json({ message: "Signup successful" });
 });
-
 /*
-get the data in json format
+get the data in json format sign up details
 */
 app.get("/signup", (request, response) => {
  
    const jsondata = request.body;
+   
+
    response.send({ jsondata });
 });
+
+

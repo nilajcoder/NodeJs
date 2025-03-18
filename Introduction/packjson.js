50e042e10f87213f72b61a9f7fb79fdb22beef14
Nilaj Chakraborty <91656802+nilajcoder@users.noreply.github.com>
2025-03-18 15:23:27 +0000

    package

---
+++ Introduction/packjson.js
@@ -0,0 +1,50 @@
+
+/*
+
+
+1. npm init to create package.json file
+
+ {
+  
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  }
+}
+this is also valid 
+
+
+
+2. {
+  
+  "scripts": {
+    "hi": "ls"
+  }
+}
+
+$ npm run hi
+
+> hi
+> ls
+
+hs_err_pid12632.log  package.json  replay_pid12632.log
+
+
+3. {
+  
+  "scripts": {
+    "hi": "ls",
+     "start":"whoami"
+
+  }
+}
+$ npm run start
+
+> start
+> whoami
+
+nilaj
+
+
+
+
+*/

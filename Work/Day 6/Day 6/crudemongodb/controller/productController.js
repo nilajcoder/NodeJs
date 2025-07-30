8724333fa61ed7141b3aa6791eb4358b857702e0
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-07-30 23:25:45 +0530

    MongooseCrudOp

---
+++ Work/Day 6/Day 6/crudemongodb/controller/productController.js
@@ -0,0 +1,121 @@
+
+
+import Product from '../model/product.js'
+
+
+export async function listProduct(req, res) {
+
+    const products = await Product.find({}).exec()
+
+    res.json({
+        status: true,
+        data: products
+
+    })
+
+}
+
+export async function getProductById(req, res) {
+    try {
+        const productId = req.params.id;
+        const product = await Product.findById(productId);
+
+        if (!product) {
+            return res.status(404).json({
+                status: false,
+                message: 'Product not found'
+            });
+        }
+
+        res.json({
+            status: true,
+            data: product
+        });
+    } catch (e) {
+        res.status(500).json({
+            status: false,
+            message: e.message
+        });
+    }
+}
+
+
+
+
+export async function deleteProduct(req, res) {
+    try {
+        const productId = req.params.id;
+        const product = await Product.findById(productId);
+
+        if (!product) {
+            return res.status(404).json({
+                status: false,
+                message: 'Product not found'
+            });
+        }
+
+        await Product.findByIdAndDelete(productId);
+
+        res.json({
+            status: true,
+            message: 'product deleted successfully'
+        });
+    } catch (e) {
+        res.status(500).json({
+            status: false,
+            message: e.message
+        });
+    }
+}
+
+
+
+
+export async function createProduct(req, res) {
+
+    try {
+        // your logic here
+
+        await Product.create(req.body);
+
+        res.json({
+            status: true,
+            message: 'product created successfully'
+        });
+
+    } catch (e) {
+        res.status(500).json({
+            status: false,
+            message: e.message
+        });
+    }
+
+}
+
+
+
+export async function updateProduct(req, res) {
+    try {
+        const productId = req.params.id;
+        const product = await Product.findById(productId);
+
+        if (!product) {
+            return res.status(404).json({
+                status: false,
+                message: 'Product not found'
+            });
+        }
+
+        await Product.findByIdAndUpdate(productId, req.body);
+
+        return res.json({
+            status: true,
+            message: 'Product updated successfully'
+        });
+    } catch (e) {
+        res.status(500).json({
+            status: false,
+            message: e.message
+        });
+    }
+}
